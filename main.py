3207ee4b037af90fbb91769408d2b814d24c0f6a
Wasim Rana <wrana697@gmail.com>
2019-10-31 03:58:33 +0600

    New codes added

--- main.py
+++ main.py
@@ -24,3 +24,30 @@ emp_2 = Employee('David', 'Smith', 70000)
 # print('Wasim'.__len__())
 
 print(emp_1.fullname())
+
+
+class Employee:
+    # class instance
+    def __init__(self, fname, lname, salary):
+        self.fname = fname
+        self.lname = lname
+        self.salary = salary
+        self.email = fname + '_' + lname + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.fname, self.lname)
+
+    def salary_info(self):
+        return "{} {}'s salary is {}".format(self.fname, self.lname, self.salary)
+
+
+emp_1 = Employee('Wasim', 'Rana', 5000)
+emp_2 = Employee('David', 'Smith', 7000)
+
+print(emp_1.fullname())
+print(emp_1.salary_info())
+print(emp_1.email)
+print('================')
+print(emp_2.fullname())
+print(emp_2.salary_info())
+print(emp_2.email)
